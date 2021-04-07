00ab0fd4bc55c1c8c70104623ce51b971e3a6b26
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-07 12:04:06 -0600

    Add deleteReservation functionality to cancel button

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -2,24 +2,16 @@ import React from 'react';
 import './Card.css';
 import PropTypes from 'prop-types';
 
-const Card = ({ id, name, date, time, number}) => {
+const Card = ({ id, name, date, time, number, deleteReservation}) => {
   return (
     <div className="card">
       <p id={name} className="name">{name}</p>
       <p>{date}</p>
       <p>{time}</p>
       <p>Number of guests: {number}</p>
-      <button>Cancel</button>
+      <button onClick={() => deleteReservation(id)}>Cancel</button>
     </div>
   )
 }
-
-// Card.propTypes = {
-//   id: PropTypes.number.isRequired,
-//   name: PropTypes.string.isRequired,
-//   date: PropTypes.string.isRequired,
-//   time: PropTypes.string.isRequired,
-//   number: PropTypes.number.isRequired
-// }
 
 export default Card;
