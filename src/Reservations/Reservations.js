4eeee1563c5477fac81cfb499410ad4ff84060f3
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-07 10:44:20 -0600

    Add PropTypes to Reservations component

--- src/Reservations/Reservations.js
+++ src/Reservations/Reservations.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Reservactions.css';
 import Card from '../Card/Card.js'
+import PropTypes from 'prop-types';
 
 const Reservations = ({ allReservations}) => {
   console.log(allReservations);
@@ -22,6 +23,14 @@ const Reservations = ({ allReservations}) => {
     {printedReservations}
     </div>
   )
+}
+
+Reservations.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  time: PropTypes.string.isRequired,
+  number: PropTypes.number.isRequired
 }
 
 export default Reservations;
