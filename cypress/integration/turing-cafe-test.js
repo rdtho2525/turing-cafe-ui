15bd0ef02bc7bcf25e751a1cc315d8f1124f7397
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-07 11:39:51 -0600

    Assert that each form input can hold value

--- cypress/integration/turing-cafe-test.js
+++ cypress/integration/turing-cafe-test.js
@@ -4,18 +4,21 @@ describe('Turing Cafe', () => {
     cy.visit('http://localhost:3000/')
   });
 
-  it.only('should display a header, a form, and all current reservations', () => {
+  it('should display a header, a form, and all current reservations', () => {
     cy.get('div[class="App"]').contains('Turing Cafe Reservations')
     cy.get('form').should('be.visible')
     cy.get('div[class="card"]').should('be.visible')
   });
 
   it('should accept and hold user input as values within a controlled form', () => {
-    
+    cy.get('input[name=name]').type('Yoda')
+    cy.get('input[name=time]').type('3:30')
+    cy.get('input[name=date]').type('4/1')
+    cy.get('input[name=numOfGuests]').type(13)
   });
 
-  it('should add a new card to the dashboard when a reservation form is submitted', () => {
-
+  it.skip('should add a new card to the dashboard when a reservation form is submitted', () => {
+    
   });
 
 })
