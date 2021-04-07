36756d4aba855ff4d4a28cfed6892cc9d2612242
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-07 11:03:21 -0600

    Add addReservation method, pass down to Form component

--- src/App/App.js
+++ src/App/App.js
@@ -21,12 +21,33 @@ class App extends Component {
       .catch(error => console.log(error))
   }
 
+  addReservation = (newRes) => {
+    fetch('http://localhost:3001/api/v1/reservations', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(
+        {
+          id: newRes.id,
+          name: newRes.name,
+          date: newRes.date,
+          time: newRes.time,
+          number: newRes.numOfGuests
+        }
+      )
+    })
+    .then(response => response.json())
+    .then(() => this.setState({ allReservations: [...this.state.allReservations, newRes] }))
+    .catch(error => console.log(error));
+  }
+
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-          <Form />
+          <Form addReservation={this.addReservation}/>
         </div>
         <div className='resy-container'>
           <Reservations allReservations={this.state.allReservations}/>
