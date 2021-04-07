d31e308280b1612e5ad930b43ba50bb756212ff3
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-07 11:33:44 -0600

    Assert that all required elements are visible

--- cypress/integration/turing-cafe-test.js
+++ cypress/integration/turing-cafe-test.js
@@ -4,5 +4,18 @@ describe('Turing Cafe', () => {
     cy.visit('http://localhost:3000/')
   });
 
-  it('should ')
+  it.only('should display a header, a form, and all current reservations', () => {
+    cy.get('div[class="App"]').contains('Turing Cafe Reservations')
+    cy.get('form').should('be.visible')
+    cy.get('div[class="card"]').should('be.visible')
+  });
+
+  it('should accept and hold user input as values within a controlled form', () => {
+    
+  });
+
+  it('should add a new card to the dashboard when a reservation form is submitted', () => {
+
+  });
+
 })
