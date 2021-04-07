638a44d3eb3e7ecec83292deb09191c78b1a9711
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-07 10:20:04 -0600

    Fetch, setState of reservationData

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import Reservations from '../Reservations/Reservations.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      allReservations: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/reservations')
+      .then(response => response.json())
+      .then(reservationsData => {
+        // console.log(reservationsData)
+        this.setState({ allReservations: reservationsData })
+      })
+      .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,7 +28,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-          
+          <Reservations allReservations={this.state.allReservations}/>
         </div>
       </div>
     )
